4cb93f399f1910e9de94d606c14ef505b4686861
Jasveer Singh <jasveer.singh.aulakh@gmail.com>
2018-08-29 21:49:21 +0200

    Wikipedia Search App

---
+++ wiki.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+  $("#search").click(function(){   
+    var searchItem = $("#searchItem").val();   
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchItem +"&format=json&callback=?"; 
+    $.ajax({
+      type:"GET",
+      url:url,
+      async:false,
+      dataType:"json",
+      success: function(data, ){
+        $("#output").html('');
+        for(var i=0; i<data[1].length; i++){
+         $("#output").prepend("<li><a href= "+data[3][i]+">"+data[1][i] +"</a><p>"+data[2][i]+"</p></li>");     
+        }
+        $("#searchItem").val('');
+      },
+      error: function(errorMessage){
+        alert("Error");
+      }
+    });
+    $("#searchItem").keypress(function(e){
+          if(e.which===13){
+            $("#search").click(); 
+            
+          }
+    })                             
+  });
+     
+});
